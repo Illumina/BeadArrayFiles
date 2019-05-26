fb32598bf7acd92ce7290b50de3e7926549202af
Jake Zieve <jzieve@illumina.com>
2019-05-26 08:39:52 -0700

    Switching subtract to mod to accomodate norm_ids larger than 512

--- module/BeadPoolManifest.py
+++ module/BeadPoolManifest.py
@@ -118,9 +118,8 @@ class BeadPoolManifest(object):
             for locus_idx in xrange(self.num_loci):
                 self.normalization_ids[locus_idx] += 100 * \
                     self.assay_types[locus_idx]
-                # To mimic with byte-wrapping behavior from GenomeStudio, AutoCall, IAAP
-                if self.normalization_ids[locus_idx] >= 256:
-                   self.normalization_ids[locus_idx] -= 256
+                # To mimic the byte-wrapping behavior from GenomeStudio, AutoCall, IAAP take the mod of 256
+                self.normalization_ids[locus_idx] %= 256
                 all_norm_ids.add(self.normalization_ids[locus_idx])
             sorted_norm_ids = sorted(all_norm_ids)
             lookup_dictionary = {}
